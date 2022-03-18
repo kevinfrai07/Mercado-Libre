7341ef6fff09aa057a9cc0cf342cbcefd8a7ec91
Kevin Fraile <kefraile@nexos-software.com.co>
2022-03-18 00:43:35 -0500

    se agrega SEO con ngUniversal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,7 @@ import { FormsModule } from '@angular/forms';
     AppComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
